f9b94becd748070b0fbfc52eda0659f611862913
Koumudi-02 <84725464+Koumudi-02@users.noreply.github.com>
2022-09-13 00:39:14 +0530

    Update CyanMagentaYellow.java

--- CyanMagentaYellow.java
+++ CyanMagentaYellow.java
@@ -15,14 +15,14 @@ public class CyanMagentaYellow {
 		for(int i = 0; i <img1.getHeight(); i++) {
 			for(int j = 0; j < img1.getWidth(); j++) {
 				int p = img1.getRGB(j, i);
-				int r = 0xff & p >> 24;
-				int g = 0xff & p >> 16;
-				int b = 0xff & p >> 8;
-				int a = 0xff & p;
-				if(g + 130 < 255)
-					g += 130;
-				if(b + 100 < 255)
-					b += 100;
+				int a = 0xff & p >> 24;
+				int r = 0xff & p >> 16;
+				int g = 0xff & p >> 8;
+				int b = 0xff & p;
+				if(g + 50 < 255)
+					g += 50;
+				if(b + 70 < 255)
+					b += 70;
 				p = a << 24 | r << 16 | g << 8 | b;
 				img1.setRGB(j, i, p);
 			}
@@ -35,14 +35,14 @@ public class CyanMagentaYellow {
 		for(int i = 0; i <img2.getHeight(); i++) {
 			for(int j = 0; j < img2.getWidth(); j++) {
 				int p = img2.getRGB(j, i);
-				int r = 0xff & p >> 24;
-				int g = 0xff & p >> 16;
-				int b = 0xff & p >> 8;
-				int a = 0xff & p;
-				if(r + 100 < 255)
-					r += 100;
-				if(b + 70 < 255)
-					b += 70;
+				int a = 0xff & p >> 24;
+				int r = 0xff & p >> 16;
+				int g = 0xff & p >> 8;
+				int b = 0xff & p;
+				if(r + 70 < 255)
+					r += 70;
+				if(b + 90 < 255)
+					b += 90;
 				p = a << 24 | r << 16 | g << 8 | b;
 				img2.setRGB(j, i, p);
 			}
@@ -55,14 +55,14 @@ public class CyanMagentaYellow {
 		for(int i = 0; i <img3.getHeight(); i++) {
 			for(int j = 0; j < img3.getWidth(); j++) {
 				int p = img3.getRGB(j, i);
-				int r = 0xff & p >> 24;
-				int g = 0xff & p >> 16;
-				int b = 0xff & p >> 8;
-				int a = 0xff & p;
-				if(r + 110 < 255)
-					r += 110;
-				if(g + 120 < 255)
-					g += 120;
+				int a = 0xff & p >> 24;
+				int r = 0xff & p >> 16;
+				int g = 0xff & p >> 8;
+				int b = 0xff & p;
+				if(r + 70 < 255)
+					r += 70;
+				if(g + 80 < 255)
+					g += 80;
 				p = a << 24 | r << 16 | g << 8 | b;
 				img3.setRGB(j, i, p);
 			}
